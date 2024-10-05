2ef008874625288a547aea9b3ac9085cbf3251a9
Arseniy0xff <arseniy.lukyanov@gmail.com>
2024-10-05 10:13:06 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -3,6 +3,8 @@ function expandDiv() {
     div.classList.add('exp-0');
 	const div_o = document.getElementById('search-section');
     div_o.classList.add('vis');
+	const div_i = document.getElementById('info-list');
+    div_i.classList.add('blur');
     // Если хотите добавлять подсказки через скрипт:
     // const suggestions = document.createElement('div');
     // suggestions.classList.add('suggestions');
@@ -15,6 +17,8 @@ function shrinkDiv() {
     div.classList.remove('exp-0');
 	const div_o = document.getElementById('search-section');
     div_o.classList.remove('vis');
+	const div_i = document.getElementById('info-list');
+    div_i.classList.remove('blur');
     // Удаляем блок с подсказками
     // const suggestions = document.querySelector('.suggestions');
     // if (suggestions) {
