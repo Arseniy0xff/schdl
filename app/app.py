80b86237c2ca6cef9334bed625240b9f93414159
Arseniy <arseniy.lukyanov@gmail.com>
2024-11-11 14:23:30 +0300

    add exm

--- app/app.py
+++ app/app.py
@@ -1,17 +1,25 @@
-# ТЕБЕ СЮДА НЕ НАДО !!!
-
 from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-
+# Список доступных API с их методами
 API_LIST = {
-    1: 'http://services.niu.ranepa.ru/API/public/teacher/teachersAndGroupsList',
-    2: 'http://example.com/api/anotherEndpoint',
+    1: {
+        'url': 'http://services.niu.ranepa.ru/API/public/teacher/teachersAndGroupsList',
+        'method': 'GET'
+    },
+    2: {
+        'url': 'http://services.niu.ranepa.ru/API/public/teacher/getSchedule', 
+        'method': 'POST'
+    },
+    3: {
+        'url': 'http://services.niu.ranepa.ru/API/public/group/getSchedule', 
+        'method': 'POST'
+    },
 }
 
-@app.route('/api/proxy', methods=['GET'])
+@app.route('/api/proxy', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def proxy():
     url = request.args.get('url')
     number = request.args.get('number')
@@ -19,14 +27,25 @@ def proxy():
     # Проверяем, передан ли URL или число
     if url:
         target_url = url
+        method = request.method  # Используем метод запроса
     elif number and number.isdigit() and int(number) in API_LIST:
-        target_url = API_LIST[int(number)]
+        target_url = API_LIST[int(number)]['url']
+        method = API_LIST[int(number)]['method']
     else:
         return jsonify({'error': 'Invalid input. Provide either a valid URL or a number.'}), 400
 
     try:
-        # Перенаправляем запрос к целевому API
-        response = requests.get(target_url)
+        # Перенаправляем запрос к целевому API в зависимости от метода
+        if method == 'GET':
+            response = requests.get(target_url, params=request.args)
+        elif method == 'POST':
+            response = requests.post(target_url, json=request.get_json())
+        elif method == 'PUT':
+            response = requests.put(target_url, json=request.get_json())
+        elif method == 'DELETE':
+            response = requests.delete(target_url, json=request.get_json())
+        else:
+            return jsonify({'error': 'Unsupported method.'}), 405
 
         # Возвращаем ответ клиенту
         return (response.content, response.status_code, {'Content-Type': response.headers['Content-Type']})
