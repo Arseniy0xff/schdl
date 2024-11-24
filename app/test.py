ee204b57a608aa83fc6d11ab27144d5510e5c299
Arseniy <arseniy.lukyanov@gmail.com>
2024-11-24 23:00:46 +0300

    test

--- app/test.py
+++ app/test.py
@@ -19,28 +19,42 @@
 #     app.run(debug=True)
 
 
+# import requests
+
+# url = 'http://services.niu.ranepa.ru/API/public/group/getSchedule'
+# data = {
+#     'id': '19979',
+#     'dateBegin': '09.11.2024',
+#     'dateEnd': '25.11.2024'
+# }
+
+# # Заголовки
+# headers = {
+#     'Content-Type': 'application/x-www-form-urlencoded',
+#     'Connection': 'Keep-Alive',
+#     'Accept-Encoding': 'gzip',
+#     'User-Agent': 'okhttp/3.4.1'
+# }
+
+# # Выполнение POST-запроса с заголовками
+# response = requests.post(url, data=data, headers=headers)
+
+# # Проверка статуса ответа
+# if response.status_code == 200:
+#     print('Успех:', response.json())  # Если ответ в формате JSON
+# else:
+#     print('Ошибка:', response.status_code, response.text)
+
+
+
+
 import requests
 
-url = 'http://services.niu.ranepa.ru/API/public/group/getSchedule'
-data = {
-    'id': '19979',
-    'dateBegin': '09.11.2024',
-    'dateEnd': '25.11.2024'
+proxy = {
+	'http': '203.142.77.226:8080',
+    'https': '203.142.77.226:8080',
 }
 
-# Заголовки
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Connection': 'Keep-Alive',
-    'Accept-Encoding': 'gzip',
-    'User-Agent': 'okhttp/3.4.1'
-}
-
-# Выполнение POST-запроса с заголовками
-response = requests.post(url, data=data, headers=headers)
-
-# Проверка статуса ответа
-if response.status_code == 200:
-    print('Успех:', response.json())  # Если ответ в формате JSON
-else:
-    print('Ошибка:', response.status_code, response.text)
+external_api_url = 'https://google.com/'
+response = requests.get(external_api_url, proxies=proxy)
+print(response)
