3d4b35a1600767cf0c40d77daabc9e87e96d16b1
Arseniy0xff <arseniy.lukyanov@gmail.com>
2024-11-28 08:46:15 +0300

    transition to service

--- namespace.js
+++ namespace.js
@@ -1,5 +1,5 @@
 const SEARCH_LIMIT = ['number', 'subject', 'teacher', 'group', 'type', 'kf', 'nf'];
 const LOADING_RANGE = 30; // Days
 
-const USE_SERVICE = false;
-const SERVICE_URL = 'http://127.0.0.1:5000/proxy';
+const USE_SERVICE = true;
+const SERVICE_URL = 'https://maiiiiin-ijztsisbf-uglstphns-projects.vercel.app/proxy'; //'http://127.0.0.1:5000/proxy';
