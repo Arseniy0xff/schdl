d2e45ab9df707b9094d52b09caedbaf903b464ac
Arseniy0xff <arseniy.lukyanov@gmail.com>
2024-11-28 09:23:48 +0300

    test

--- namespace.js
+++ namespace.js
@@ -2,4 +2,4 @@ const SEARCH_LIMIT = ['number', 'subject', 'teacher', 'group', 'type', 'kf', 'nf
 const LOADING_RANGE = 30; // Days
 
 const USE_SERVICE = true;
-const SERVICE_URL = 'https://maiiiiin-ijztsisbf-uglstphns-projects.vercel.app/proxy'; //'http://127.0.0.1:5000/proxy';
+const SERVICE_URL = 'https://maiiiiin.vercel.app/proxy'; //'http://127.0.0.1:5000/proxy';
